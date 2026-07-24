41f8bb8cbea6e2d28fd0f9d25b1ccdbc0a1f8510
Jeffrey <125141643+sootysplash@users.noreply.github.com>
2026-07-24 19:27:25 +1000

    port to 1.21-.6

--- src/main/java/me/sootysplash/vai/mixin/GuiGraphicsExtractorMixin.java
+++ src/main/java/me/sootysplash/vai/mixin/GuiGraphicsExtractorMixin.java
@@ -15,7 +15,10 @@ public abstract class GuiGraphicsExtractorMixin {
     @Inject(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", at = @At("RETURN"))
     private void onItemCountOld(Font font, ItemStack itemStack, int x, int y, String countText, CallbackInfo ci) {
         GuiGraphics gg = (GuiGraphics) (Object) this;
+        gg.pose().pushPose();
+        gg.pose().translate(0.0F, 0.0F, 200.0F);
         Main.doDraw(gg, font, itemStack, x, y, countText);
+        gg.pose().popPose();
     }
 
 }
